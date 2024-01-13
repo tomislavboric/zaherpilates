d3372c5aaea0bebc6c0f4211e4c34c9945fce447
tomislavboric <tomislav.boric@gmail.com>
2024-01-13 22:05:41 +0100

    Removed title

--- page-templates/home/newsletter.php
+++ page-templates/home/newsletter.php
@@ -1,11 +1,11 @@
 <section class="newsletter section">
 	<div class="grid-container">
-		<header class="section__header section__header--center">
+		<?php /* <header class="section__header section__header--center">
 			<h2 class="section__title">Newsletter</h2>
 			<div class="section__desc">
 				<p>U tjednom newsletteru s tobom dijelim...</p>
 			</div>
-		</header>
+		</header> */ ?>
 		<?php // Mailerlite Newsletter Form ?>
 		<div class="ml-embedded" data-form="sPVPsN"></div>
 	</div>
