aa9b658becf01ee7c3e7ac2ceb3e72676e538fac
tomislavboric <tomislav.boric@gmail.com>
2024-01-13 22:08:25 +0100

    updates

--- page-templates/home/newsletter.php
+++ page-templates/home/newsletter.php
@@ -1,6 +1,6 @@
-<section class="newsletter section section--small">
+<section class="newsletter section">
 	<div class="grid-container">
-		<header class="section__header section__header--center">
+		<header class="section__header section__header--tiny section__header--center">
 			<h2 class="section__title">Newsletter</h2>
 			<div class="section__desc">
 				<p>U tjednom newsletteru s tobom dijelim...</p>
