7a3e80aae4aa7f8df94e7e4df97abbf972ca16b4
tomislavboric <tomislav.boric@gmail.com>
2025-01-08 18:21:33 +0100

    Updates

--- page-templates/home/about.php
+++ page-templates/home/about.php
@@ -7,13 +7,12 @@
 					<div class="section__subtitle">O meni</div>
 					<h2 class="section__title">Ivana Zaher</h2>
 					<div class="section__desc">
-						<p>Hej! Ja sam Ivana, pilates instruktorica, poduzetnica i majka dvoje dječice. Odrasla sam u Metkoviću, studirala u Zagrebu i sada živim u Zurichu.</p>
+						<p>Hej! Ja sam Ivana, pilates instruktorica, poduzetnica i majka dvoje dječice.</p>
 					</div>
 				</header>
 				<p>Vodila sam stotine žena u oporavku nakon poroda, povratku snage i samopouzdanja u tijelo. Radimo na promjeni i stvaranju održivog odnosa sa tjelovježbom.</p>
 				<p>Upravo prolazak kroz dva intenzivna postpartuma gurnula su me prvo u vlastitu rehabilitaciju nakon trudnoće a onda i potaknula da svoje znanje i iskustvo podijelim kroz postpartum programe.</p>
 				<p>Zbog PCOS-a trebala sam treninge koji su prilagođeni trenutnoj fazi ciklusa u kojoj se nalazim. Upravo zbog gustog rasporeda i hrpu uloga koje na dnevnoj bazi imam, briga o sebi postala mi je esencijalna kako bih se za svoje ukućane, prijatelje i klijentice svakodnevno pojavljivala 'svježa'. Iz ove vlastite potrebe nastao je LOOP koji nudi kratke treninge prilagođene fazama menstrualnog ciklusa te u sklopu kojeg organiziram gostovanja na temu ženskog zdravlja.</p>
-				<p>Sve svoje trenutne procese, privatne i poslovne, dijelim u newsletteru četvrtkom ujutro. Prijavom na Newsletter saznaješ prva za promocije i novosti na platformama.</p>
 			</div>
 
 			<div class="about__image">
