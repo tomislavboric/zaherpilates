b5ffa8180081bc9e813222bb0226b7cbb3f4c1e6
tomislavboric <tomislav.boric@gmail.com>
2025-03-31 18:20:31 +0200

    Updates

--- page-templates/loop/hero.php
+++ page-templates/loop/hero.php
@@ -11,7 +11,7 @@
 				</div>
 			</header>
 			<div class="hero__cta">
-				<a href="<?php echo home_url(); ?>/cjenik/" class="button button--large">Isprobaj LOOP besplatno</a>
+				<a href="<?php echo home_url(); ?>/cjenik/" class="button button--large">Priključi se LOOPu</a>
 			</div>
 		</div>
 	</div>
