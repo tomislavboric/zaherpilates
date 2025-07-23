8797304b6c67163a0843dc1091f31073d3f9d4dd
tomislavboric <tomislav.boric@gmail.com>
2025-07-23 13:32:45 +0200

    Sync with server

--- functions.php
+++ functions.php
@@ -105,3 +105,30 @@ function remove_admin_bar() {
 add_filter( 'rank_math/metabox/priority', function( $priority ) {
 	return 'low';
 });
+
+// GTM u <head>
+add_action('wp_head', 'zaher_add_gtm_head', 1);
+function zaher_add_gtm_head() {
+  ?>
+  <!-- Google Tag Manager -->
+  <script>
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-TZGV2HD6');
+  </script>
+  <!-- End Google Tag Manager -->
+  <?php
+}
+
+// GTM noscript u <body>
+add_action('wp_body_open', 'zaher_add_gtm_body');
+function zaher_add_gtm_body() {
+  ?>
+  <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TZGV2HD6"
+  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
+  <?php
+}
